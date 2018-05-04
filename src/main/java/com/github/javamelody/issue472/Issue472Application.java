97a567126f075225a7f19048cfde99f7bdc93393
Vladimir <v.plizga@ftc.ru>
2018-05-04 16:32:03 +0700

    Initial commit

---
+++ src/main/java/com/github/javamelody/issue472/Issue472Application.java
@@ -0,0 +1,12 @@
+package com.github.javamelody.issue472;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue472Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Issue472Application.class, args);
+  }
+}
